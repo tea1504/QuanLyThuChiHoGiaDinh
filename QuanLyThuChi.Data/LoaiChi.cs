c870390d1301e283190e2bc81613e92decdc13fd
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-05-19 16:29:19 +0700

    Tạo lớp loại chi

---
+++ QuanLyThuChi.Data/LoaiChi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThuChi.Data
+{
+    public class LoaiChi
+    {
+        private int maLoaiChi;
+        private string tenLoaiChi;
+
+        public int MaLoaiChi { get => maLoaiChi; set => maLoaiChi = value; }
+        public string TenLoaiChi { get => tenLoaiChi; set => tenLoaiChi = value; }
+    }
+}
