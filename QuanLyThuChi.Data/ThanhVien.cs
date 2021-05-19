3eafea16fb952b26f42f1fe1cdb2ca4a7a9fd526
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-05-19 16:27:09 +0700

    Tạo class ThanhVien

---
+++ QuanLyThuChi.Data/ThanhVien.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThuChi.Data
+{
+    public class ThanhVien
+    {
+        private int maThanhVien;
+        private string hoLot;
+        private string ten;
+
+        public int MaThanhVien { get => maThanhVien; set => maThanhVien = value; }
+        public string HoLot { get => hoLot; set => hoLot = value; }
+        public string Ten { get => ten; set => ten = value; }
+    }
+}
