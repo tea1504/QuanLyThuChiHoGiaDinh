6d1815f1f4ee2bf466d4094065423e2d45e3b3c2
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2021-05-19 16:37:10 +0700

    Tạo lớp Chi

---
+++ QuanLyThuChi.Data/Chi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyThuChi.Data
+{
+    public class Chi
+    {
+        private int maThanhVien;
+        private int maLoaiChi;
+        private int maTaiKhoan;
+        private DateTime ngayChi;
+        private float soTienChi;
+        private string ghiChuChi;
+
+        public int MaThanhVien { get => maThanhVien; set => maThanhVien = value; }
+        public int MaLoaiChi { get => maLoaiChi; set => maLoaiChi = value; }
+        public int MaTaiKhoan { get => maTaiKhoan; set => maTaiKhoan = value; }
+        public DateTime NgayChi { get => ngayChi; set => ngayChi = value; }
+        public float SoTienChi { get => soTienChi; set => soTienChi = value; }
+        public string GhiChuChi { get => ghiChuChi; set => ghiChuChi = value; }
+    }
+}
